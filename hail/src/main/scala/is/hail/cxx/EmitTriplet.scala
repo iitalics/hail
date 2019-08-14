1ad078046d5806e2d739118efb13049498994550
Milo Turner <iitalics@gmail.com>
2019-08-14 11:18:00 -0400

    ArrayEmitter refactor
    emit => consume

--- hail/src/main/scala/is/hail/cxx/EmitTriplet.scala
+++ hail/src/main/scala/is/hail/cxx/EmitTriplet.scala
@@ -90,10 +90,6 @@ object SparkFunctionContext {
 
 case class SparkFunctionContext(sparkEnv: Code, fs: Code, region: EmitRegion)
 
-abstract class ArrayEmitter(val setup: Code, val m: Code, val setupLen: Code, val length: Option[Code], val arrayRegion: EmitRegion) {
-  def emit(f: (Code, Code) => Code): Code
-}
-
 object NDArrayEmitter {
   def broadcastFlags(fb: FunctionBuilder, nDims: Int, shape: Code): Seq[Variable] = {
     val broadcasted = 0
